87cc3587b75f891af895d0b48f3430b1007b4817
nvidia tx201 <nvidia@tx201.com>
2017-06-14 16:17:38 -0400

    Thresh slider does hough lines

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ LDFLAGS = -I/usr/include/opencv -lopencv_cudabgsegm -lopencv_cudaobjdetect -lope
 
 build: shortSampleCpu_v2 shortSampleCpu_v3 shortSampleGpu_v3 ThreshSliders_v3
 
+#######################     CPU       ############################
 shortSampleCpu_v2.o: shortSampleCpu_v2.cpp
 	g++ -std=c++11 -c shortSampleCpu_v2.cpp -o shortSampleCpu_v2.o
 
@@ -21,7 +22,7 @@ ThreshSliders_v3.o: ThreshSliders_v3.cpp
 ThreshSliders_v3: ThreshSliders_v3.o
 	g++ -std=c++11 ThreshSliders_v3.o -o ThreshSliders_v3 $(LDFLAGS)
 
-###############################################################################
+#######################     CUDA       ############################
 CUDAFLAGS= -std=c++11 -m64 -gencode arch=compute_62,code=compute_62
 LIBS= -lpthread -lcudart -lcublas
 LIBDIRS=-L/usr/local/cuda-8.0/lib64
@@ -40,6 +41,7 @@ shortSampleGpu_v3.o: shortSampleGpu_v3.cu
 shortSampleGpu_v3: shortSampleGpu_v3.o
 	nvcc -ccbin g++ $(CUDAFLAGS) shortSampleGpu_v3.o -o shortSampleGpu_v3 $(LIBDIRS) $(LIBS) $(LDFLAGS)
 
+########################    CLEAN   ################################
 clean:
 	-rm -f shortSampleCpu_v2.o
 	-rm -f shortSampleCpu_v2
@@ -49,3 +51,6 @@ clean:
 	-rm -f shortSampleGpu_v2
 	-rm -f shortSampleGpu_v3.o
 	-rm -f shortSampleGpu_v3
+	-rm -f ThreshSliders_v3.o
+	-rm -f ThreshSliders_v3
+	
--- ThreshSliders_v3.cpp
+++ ThreshSliders_v3.cpp
@@ -4,13 +4,23 @@
 #include <stdlib.h>
 #include <stdio.h>
 using namespace cv;
-int threshold_value = 0;
+int threshold_value = 145;
 int threshold_type = 3;
-int kSize =1;
+int erosion_size = 1;
+int ekSize =1;
+int bkSize =3;
+int h_rho = 1; // Distance resolution of the accumulator in pixels (hough transform)
+int h_theta = 180; // Angle resolution of the accumulator in radians (hough transform)
+int h_thresh = 5; // Accumulator threshold parameter. Only those lines are returned that get enough votes (hough transform)
+int h_minLineLen = 10; // Line segments shorter than that are rejected (hough transform)
+int h_maxLineGap = 7; // Maximum allowed gap between points on the same line to link them (hough transform)
+
+std::vector<cv::Vec4i> lines;
+
 int const max_value = 255;
 int const max_type = 4;
 int const max_BINARY_value = 255;
-Mat src, src_gray, dst_gray,dst;
+Mat src, src_gray, src_blur, src_erode, src_canny, dst;
 const char* window_name = "Threshold Demo";
 const char* trackbar_type = "Type: \n 0: Binary \n 1: Binary Inverted \n 2: Truncate \n 3: To Zero \n 4: To Zero Inverted";
 const char* trackbar_value = "Value";
@@ -20,7 +30,6 @@ int main( int, char** argv )
   //src = imread( argv[1], 1 );
 
   src = cv::imread("file.png");
-  cvtColor( src, src_gray, COLOR_RGB2GRAY );
   namedWindow( window_name, WINDOW_AUTOSIZE );
   createTrackbar( trackbar_type,
                   window_name, &threshold_type,
@@ -29,8 +38,11 @@ int main( int, char** argv )
                   window_name, &threshold_value,
                   max_value, Threshold_Demo );
   createTrackbar( "Blur kSize" ,
-                  window_name, &kSize,
-                  5, Threshold_Demo );
+                  window_name, &bkSize,
+                  9, Threshold_Demo );
+  createTrackbar( "Erode kSize" ,
+                  window_name, &ekSize,
+                  9, Threshold_Demo );
   Threshold_Demo( 0, 0 );
   for(;;)
     {
@@ -48,8 +60,40 @@ void Threshold_Demo( int, void* )
      3: Threshold to Zero
      4: Threshold to Zero Inverted
    */
-  threshold( src_gray, dst_gray, threshold_value, max_BINARY_value,threshold_type );
-  GaussianBlur(dst_gray, dst, cv::Size((kSize*2)+1, (kSize*2)+1), 0.0, 0.0,
-        cv::BORDER_DEFAULT);
+   
+   /*
+     *  It Uses the following algorithm to find white lines:
+     *     1. blur the image
+     *     2. turn image into grayscale
+     *     3. run it through a threshold filter using THRESH_TO_ZERO mode
+     *     4. run it through an erosion filter
+     *     5. run it through a Canny edge detector
+     *     6. finally, take this processed image and find the lines using   
+   */
+   // Blur the image
+    GaussianBlur(src, src_blur, cv::Size((bkSize*2)+1, (bkSize*2)+1), 0.0, 0.0, cv::BORDER_DEFAULT);   
+   // GrayScale the image     
+    cvtColor( src_blur, src_gray, COLOR_RGB2GRAY );
+   // Threshhold the image
+    threshold( src_gray, src_erode, threshold_value, max_BINARY_value,threshold_type );
+    // Erode the image
+    cv::Mat element = getStructuringElement(cv::MORPH_ELLIPSE, cv::Size((ekSize*2)+1, (ekSize*2)+1),
+                                            cv::Point(-1, -1));
+    cv::erode(src_erode, src_canny, element);    
+    // Canny edge detection
+    cv::Canny(src_canny, dst, 50, 250, 3);
+    
+    // Find the Hough lines
+    cv::HoughLinesP(src_canny, lines, h_rho,
+        (CV_PI / h_theta), h_thresh, h_minLineLen,
+        h_maxLineGap);
+    Mat hough_image = cv::Mat::zeros(src_canny.size(), src_canny.type());
+        // Draw the Hough lines on the image
+    for (int i = 0; i < lines.size(); i++) {
+        line(hough_image, cv::Point(lines[i][0], lines[i][1]),
+            cv::Point(lines[i][2], lines[i][3]), cv::Scalar(255, 255, 255), 3, 8);
+    }
+  
+  imshow("HoughLines", hough_image);
   imshow( window_name, dst );
 }
